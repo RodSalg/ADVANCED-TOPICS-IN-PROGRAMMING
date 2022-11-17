68f08f206f4ebb69a82fe4c07d13c7edc84913e3
Thiago Rodrigo Monteiro Salgado <88393946+RodSalg@users.noreply.github.com>
2022-11-16 21:14:57 -0400

    deletando os docs

--- Trabalhos do laboratório/Lab. 4/JediProject/doc/member-search-index.js
+++
@@ -1,1 +0,0 @@
-memberSearchIndex = [{"p":"<Unnamed>","c":"SessaoJediMain","l":"main(String[])","u":"main(java.lang.String[])"},{"p":"<Unnamed>","c":"SessaoJediMain","l":"SessaoJediMain()","u":"%3Cinit%3E()"}];updateSearchResults();
--- Trabalhos do laboratório/Lab. 4/JediProject/doc/type-search-index.js
+++
@@ -1,1 +0,0 @@
-typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"<Unnamed>","l":"IniciadoJedi"},{"p":"<Unnamed>","l":"SessaoJedi"},{"p":"<Unnamed>","l":"SessaoJediMain"},{"p":"<Unnamed>","l":"TreinadorJedi"}];updateSearchResults();
